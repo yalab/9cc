a6b1bb2c15d80162a1560b395d220c9736700bcd
yalab <rudeboyjet@gmail.com>
2019-10-20 21:50:48 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -21,13 +21,19 @@ struct Token {
 };
 
 Token *token;
+char* user_input;
 
-void error(char *fmt, ...)  {
-	va_list ap;
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
-	exit(1);
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
 }
 
 bool consume(char op) {
@@ -40,14 +46,14 @@ bool consume(char op) {
 
 void expect(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op) {
-		error("'%c'ではありません", op);
+		error_at(token->str, "'%c'ではありません", op);
 	}
 	token = token->next;
 }
 
 int expect_number() {
 	if (token->kind != TK_NUM) {
-		error("数ではありません");
+		error_at(token->str, "数ではありません");
 	}
 	int val = token->val;
 	token = token->next;
@@ -66,7 +72,8 @@ Token *new_token(TokenKind kind, Token *cur, char* str) {
 	return tok;
 }
 
-Token *tokenize(char *p) {
+Token *tokenize() {
+	char *p = user_input;
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
@@ -87,7 +94,7 @@ Token *tokenize(char *p) {
 			cur->val = strtol(p, &p, 10);
 			continue;
 		}
-		error("トークナイズできません");
+		error_at(p, "トークナイズできません");
 	}
 
 	new_token(TK_EOF, cur, p);
@@ -100,7 +107,8 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	token = tokenize(argv[1]);
+	user_input = argv[1];
+	token = tokenize();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
